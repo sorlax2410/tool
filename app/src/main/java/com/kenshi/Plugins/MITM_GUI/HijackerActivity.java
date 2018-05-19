b8c56a1693f7a21e6bf76c73a89d4d95b31edc20
sorlax2410 <lional2410@yahoo.com>
2018-05-19 11:59:54 +0700

    updated

--- app/src/main/java/com/kenshi/Plugins/MITM_GUI/HijackerActivity.java
+++ app/src/main/java/com/kenshi/Plugins/MITM_GUI/HijackerActivity.java
@@ -1,16 +1,92 @@
 package com.kenshi.Plugins.MITM_GUI;
 
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.ToggleButton;
 
 import org.apache.http.impl.cookie.BasicClientCookie;
 
+import com.kenshi.Plugins.mitm.SpoofSession;
 import com.mitdroid.kenshi.Main.R;
 
 import java.util.HashMap;
 
 public class HijackerActivity extends AppCompatActivity {
+
+    public class SessionListAdapter extends ArrayAdapter<Session> {
+        private int layoutId = 0;
+        private HashMap<String, Session> sessions;
+
+        public class FacebookUserTask extends AsyncTask<Session, Void, Boolean> {
+
+            /**
+             * Override this method to perform a computation on a background thread. The
+             * specified parameters are the parameters passed to {@link #execute}
+             * by the caller of this task.
+             * <p>
+             * This method can call {@link #publishProgress} to publish updates
+             * on the UI thread.
+             *
+             * @param sessions The parameters of the task.
+             * @return A result, defined by the subclass of this task.
+             * @see #onPreExecute()
+             * @see #onPostExecute
+             * @see #publishProgress
+             */
+            @Override
+            protected Boolean doInBackground(Session... sessions) {
+                return null;
+            }
+        }
+
+        public class XdaUserTask extends AsyncTask<Session, Void, Boolean> {
+
+            /**
+             * Override this method to perform a computation on a background thread. The
+             * specified parameters are the parameters passed to {@link #execute}
+             * by the caller of this task.
+             * <p>
+             * This method can call {@link #publishProgress} to publish updates
+             * on the UI thread.
+             *
+             * @param sessions The parameters of the task.
+             * @return A result, defined by the subclass of this task.
+             * @see #onPreExecute()
+             * @see #onPostExecute
+             * @see #publishProgress
+             */
+            @Override
+            protected Boolean doInBackground(Session... sessions) {
+                return null;
+            }
+        }
+
+        public class SessionHolder {
+
+        }
+
+        /**
+         * @alert: the on request listener interface cannot be found
+         * @Note: This class will be use without the interface.
+         * TODO: write OnRequestListener interface
+         */
+        class RequestListener {
+
+        }
+
+        public SessionListAdapter(int layoutId) {
+            super(HijackerActivity.this, layoutId);
+
+            this.layoutId = layoutId;
+            sessions = new HashMap<>();
+        }
+
+    }
 
     public static class Session {
         public Bitmap picture = null;
@@ -29,6 +105,14 @@ public class HijackerActivity extends AppCompatActivity {
             return name.replaceAll("[ .\\\\/:*?\"<>|\\\\/:*?\"<>|]", "-");
         }
     }
+
+    private ToggleButton hijackToggleButton;
+    private ProgressBar hijackProgress;
+    private ListView listView;
+    private SessionListAdapter adapter;
+    private boolean running;
+    private SessionListAdapter.RequestListener requestListener;
+    private SpoofSession spoofSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/kenshi/Plugins/mitm/SpoofSession.java
@@ -0,0 +1,4 @@
+package com.kenshi.Plugins.mitm;
+
+public class SpoofSession {
+}
